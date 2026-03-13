0900dca4ae683635b0a8f32f6793b825f2be237f
Ashwini Janrao <janraoashwini4@gmail.com>
2026-03-13 15:46:01 +0530

    Initial FamilyPulse project

---
+++ constants/theme.ts
@@ -0,0 +1,18 @@
+export const Colors = {
+  light: {
+    text: "#11181C",
+    background: "#ffffff",
+    tint: "#2f95dc",
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: "#2f95dc",
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: "#ffffff",
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: "#ffffff",
+  },
+};
